e94d87ba05c88af3b9f84fa31a839d9d6a75ba8e
nic <npreda129@gmail.com>
2026-09-03 18:01:37 -0400

    implemented a search bar to filter decks with, both for convenience and as the only piece of client-rendered functionality. implemented cypress tests on this function

--- cypress/e2e/simple.cy.js
+++ cypress/e2e/simple.cy.js
@@ -1,10 +1,29 @@
-describe('Flash card server', () => {
-  // Happy PATH
-  describe('normal study session', () => {
-    it("renders a card from the deck.", () => {
-      cy.visit('/review/Deadlock');
-      cy.get('[id="front"]')
-        .should('be.visible')
-    })
+describe('Flash card server, deck selection page', () => {
+  beforeEach(() => {
+    cy.visit('/');
   })
+
+  it("renders a link to every deck", () => {
+    cy.get('li')
+      .should('be.visible')
+  })
+  
+  describe('The user types and clears a search term', () => {
+    const searchTerm = "Empty";
+    it("hides the links to non-matching decks", () => {
+      cy.get('input').type(searchTerm);
+      cy.get(`li:contains("${searchTerm}")`)
+        .should('not.be.hidden');
+      cy.get(`li:not(:contains("${searchTerm}"))`)
+        .should('be.hidden');
+
+    });
+
+    it("shows all links when the search clears", () => {
+      cy.get('input').type(searchTerm);
+      cy.get('input').clear();
+      cy.get('li')
+        .should('be.visible');
+    })
+  }) 
 })
